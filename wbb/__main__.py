86095674d2c3f62bdcd4c34734eadf95e688c542
abood1101101100-sys <abood1101101100@gmail.com>
2026-05-20 04:23:54 +0300

    Update __main__.py

--- wbb/__main__.py
+++ wbb/__main__.py
@@ -24,12 +24,12 @@ SOFTWARE.
 import asyncio
 import importlib
 import re
-from contextlib import closing, suppress
+from contextlib import suppress
 
 from pyrogram import filters, idle
 from pyrogram.enums import ChatType, ParseMode
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from uvloop import install
+
 
 import wbb
 from wbb import app, log
@@ -511,9 +511,5 @@ async def help_button(client, query):
 
 
 if __name__ == "__main__":
-    install()
-    loop = asyncio.get_event_loop()
-    with closing(loop):
-        with suppress(asyncio.exceptions.CancelledError):
-            loop.run_until_complete(start_bot())
-        loop.run_until_complete(asyncio.sleep(3.0))
+    with suppress(asyncio.exceptions.CancelledError):
+        asyncio.run(start_bot())
